accf698343d22aa4841eabdec64cefa24a327a22
Riku Niittymäki <riku.niittymaki@helsinki.fi>
2017-06-09 13:34:37 +0000

    Initial skeleton.

---
+++ src/Rikniitt/Provider/IlluminateDatabase/IlluminateDatabaseServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Rikniitt\Provider\IlluminateDatabase;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class IlluminateDatabaseServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * Register Database service.
+     *
+     * @param Pimple\Container $pimple
+     */
+    public function register(Container $Pimple)
+    {
+
+    }
+
+}
