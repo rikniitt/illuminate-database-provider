9ba5105d7b201be347ffdd0b3a9cdbaf22cddcd1
Riku Niittymäki <riku.niittymaki@helsinki.fi>
2017-06-09 18:08:39 +0300

    First draft

--- src/Rikniitt/Provider/IlluminateDatabase/IlluminateDatabaseServiceProvider.php
+++ src/Rikniitt/Provider/IlluminateDatabase/IlluminateDatabaseServiceProvider.php
@@ -14,8 +14,50 @@ class IlluminateDatabaseServiceProvider implements ServiceProviderInterface
      *
      * @param Pimple\Container $pimple
      */
-    public function register(Container $Pimple)
+    public function register(Container $pimple)
     {
+        $defaults = [
+            'database.settings.connections' => [],
+            'database.settings.global' => false,
+            'database.settings.eloquent' => false,
+        ];
+
+        $pimple['database.manager'] = function () use ($pimple, $defaults) {
+
+            foreach ($defaults as $key => $value) {
+                if (!isset($pimple[$key])) {
+                    $pimple[$key] = $value;
+                }
+            }
+
+            $database = new DB();
+
+            foreach ($pimple['database.settings.connections'] as $name => $connection) {
+                $database->addConnection($connection, $name);
+            }
+
+            if ($pimple['database.settings.global']) {
+                $database->setAsGlobal();
+            }
+
+            if ($pimple['database.settings.eloquent']) {
+                $database->bootEloquent();
+            }
+
+            return $database;
+        };
+
+        $pimple['database.connection'] = function () use ($pimple) {
+            $manager = $pimple['database.manager'];
+            return $manager->getConnection();
+        };
+        $pimple['database'] = function () use ($pimple) {
+            return $pimple['database.connection'];
+        };
+
+        $pimple['database.schema'] = function () use ($pimple) {
+            return $pimple['database.connection']->getSchemaBuilder();
+        };
 
     }
 
